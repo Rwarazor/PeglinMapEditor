bfa47321434a00cf9b45f02d4cd1511c3ebcc5f8
Георгий Асатрян <ago-2004@mail.ru>
2023-09-01 15:27:28 +0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Peglin Map Editor
+
+See https://mods.pegl.in/ for links to the Discord and more.
---
+++ src/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace PeglinMapEditor {
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Peglin.exe")]
+    public class Plugin : BaseUnityPlugin {
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+
+        private void Awake() {
+            harmony.PatchAll();
+
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
